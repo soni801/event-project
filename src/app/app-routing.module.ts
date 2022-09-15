09424df1985b2882859143de065e8e23f6b61d64
Soni <soni801_@outlook.com>
2022-09-15 17:56:41 +0200

    chore: create initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LiveViewComponent } from './live-view/live-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'live', component: LiveViewComponent },
+    { path: '', redirectTo: '/live', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LiveViewComponent } from './live-view/live-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveViewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
